6b261b2ccae94113f9dd0c83fdab678a86955447
takumakoike <takuma.151e@gmail.com>
2024-03-11 20:09:26 +0900

    liff

--- src/js/login.js
+++ src/js/login.js
@@ -1,7 +1,19 @@
-document.getElementById("getInfo").addEventListener("click", handleClick);
+// LIFFログイン
+import liff from '@line/liff';
 
-function handleClick(){
-    // console.log("クリックされました")
-    
+// LIFFアプリの初期化処理
+liff.init({
+    liffId: '2004006139-5eWAxRAO',
+}).then( () => {
 
-}
+    // 初期化完了後の処理
+    // ①ログイン
+    document.getElementById("getInfo").addEventListener("click", () => {
+        if(!liff.isLoggedIn()){
+            liff.login();
+        } else {
+            alert("ログイン済みです。ウインドウを閉じます");
+            liff.closeWindow();
+        }
+    })
+});
--- src/ts/userinfo.ts
+++
@@ -1,9 +0,0 @@
-import type { APIRoute } from "astro";
-// import { supabase } from "../db/supabase";
-
-export const POST: APIRoute = async ({ request, redirect }) => {
-    const formData = await request.formData();
-    console.log(formData);
-        
-    return redirect("/");
-};
